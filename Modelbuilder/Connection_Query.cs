628a40f9a4338e045b124f6488faf3a7918c0855
herbie68 <herbert.nijkamp@gmail.com>
2021-07-04 23:20:22 +0200

    Add project files.

---
+++ Modelbuilder/Connection_Query.cs
@@ -0,0 +1,24 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+//namespace ModelbouwBeheer
+namespace ConnectionNamespace
+{
+    public class Connection_Query
+    {
+        public MySqlConnection connection;
+        public static readonly string server = "localhost";
+        public static readonly string database = "modelbuilder";
+        public static readonly string port = "3306";
+        public static readonly string profiler = "4040";
+        public static readonly string uid = "root";
+        public static readonly string password = "admin";
+
+        public static string connectionString = "SERVER=" + server + ";PORT=" + port + ";DATABASE=" + database + ";UID=" + uid + ";PASSWORD=" + password + ";";
+    }
+}
---
+++ Modelbuilder/database.cs
@@ -0,0 +1,145 @@
+﻿using ConnectionNamespace;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelbuilder
+{
+    public class Database
+    {
+        public string TableName { get; set; }
+        public string DataTable { get; set; }
+        public string SqlCommand { get; set; }
+        public string SqlCommandString { get; set; }
+        public string SqlSelectionString { get; set; }
+        public string SqlOrderByString { get; set; }
+        public string SqlWhereString { get; set; }
+
+        public static MySqlConnection myConnection = null;
+
+        public void Connect()
+        {
+            myConnection = new MySqlConnection(Connection_Query.connectionString);
+            MySqlConnection connection = myConnection;
+
+            connection.Open();
+        }
+
+        public void Disconnect()
+        {
+            myConnection = new MySqlConnection(Connection_Query.connectionString);
+            MySqlConnection connection = myConnection;
+
+            connection.Close();
+        }
+
+        public DataTable LoadMySqlData()
+        {
+            string mySelectQuery = "SELECT * FROM " + Connection_Query.database + "." + TableName;
+            MySqlConnection myConnection = new MySqlConnection(Connection_Query.connectionString);
+
+            myConnection.Open();
+
+            var myCommand = new MySqlCommand(mySelectQuery, myConnection);
+
+            DataTable tempDatatable = new DataTable();
+            tempDatatable.Load(myCommand.ExecuteReader());
+            myConnection.Close();
+
+            return tempDatatable;
+        }
+
+        public DataTable LoadSpecificMySqlData()
+        {
+            string OrderString= "", WhereString = "";
+
+            if (SqlOrderByString != "")
+            {
+                if (SqlOrderByString != null) OrderString = " ORDER BY " + SqlOrderByString;
+            }
+            else
+            {
+                OrderString = "";
+            }
+
+            if (SqlWhereString != "")
+            {
+                if (SqlWhereString != null) WhereString = " WHERE " + SqlWhereString;
+            }
+            else
+            {
+                WhereString = "";
+            }
+
+            string mySelectQuery = "SELECT " + SqlSelectionString + " FROM " + Connection_Query.database + "." + TableName + WhereString + OrderString;
+            
+            MySqlConnection myConnection = new MySqlConnection(Connection_Query.connectionString);
+
+            myConnection.Open();
+
+            var myCommand = new MySqlCommand(mySelectQuery, myConnection);
+
+            DataTable tempDataTable = new DataTable();
+            tempDataTable.Load(myCommand.ExecuteReader());
+
+            return tempDataTable;
+        }
+        public int UpdateMySqlDataRecord()
+        {
+            MySqlCommand SqlCmdUpdate = new MySqlCommand
+            {
+                Connection = Database.myConnection,
+                CommandText = SqlCommand + Connection_Query.database + "." + TableName + SqlCommandString
+            };
+
+            MySqlConnection myConnection = new MySqlConnection(Connection_Query.connectionString);
+
+            myConnection.Open();
+
+            SqlCmdUpdate.ExecuteNonQuery();
+            long ID = SqlCmdUpdate.LastInsertedId;
+
+            myConnection.Close();
+
+            return (int)ID;
+
+        }
+        public int RetrieveSpecificIdFromMySqlData()
+        {
+            string WhereString;
+
+            if (SqlOrderByString != "")
+            {
+                WhereString = " WHERE " + SqlWhereString;
+            }
+            else
+            {
+                WhereString = "";
+            }
+
+            string myRetrieveQuery = "SELECT " + SqlCommandString + " FROM " + Connection_Query.database + "." + TableName + WhereString;
+            MySqlConnection myConnection = new MySqlConnection(Connection_Query.connectionString);
+
+            myConnection.Open();
+
+            var myCommand = new MySqlCommand(myRetrieveQuery, myConnection);
+
+            MySqlDataReader retrieveData = myCommand.ExecuteReader();
+
+            retrieveData.Read();
+            var reader = retrieveData.GetOrdinal("currency_Id");
+            
+            int ID = retrieveData.GetInt32(reader);
+            
+            myConnection.Close();
+
+            return ID;
+        }
+
+    }
+}
