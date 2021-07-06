cd54835e11ef674449228d6404b2c34487fda67a
herbie68 <herbert.nijkamp@gmail.com>
2021-07-06 16:35:17 +0200

    Completed Counbtry Page

--- Modelbuilder/metadataCountry.xaml.cs
+++ Modelbuilder/metadataCountry.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Modelbuilder
 {
@@ -20,9 +10,150 @@ namespace Modelbuilder
     /// </summary>
     public partial class metadataCountry : Page
     {
+        private readonly string DatabaseTable = "country";
+        private readonly string DatabaseCurrencyTable = "currency";
         public metadataCountry()
         {
             InitializeComponent();
+            DataContext = new CountryCodeViewModel();
+
+            Database dbConnection = new()
+            {
+                TableName = DatabaseTable
+            };
+
+            _ = new DataTable();
+
+            DataTable dtCountryCodes = dbConnection.LoadMySqlData();
+
+            // Load the data from the database into the datagrid
+            CountryCode_DataGrid.DataContext = dtCountryCodes;
+
+            // Make sure the first row in the datagrid is selected
+            CountryCode_DataGrid.SelectedIndex = 0;
+            CountryCode_DataGrid.Focus();
+        }
+        private void CommonCommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void CountryCode_DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DataGrid dataGrid = (DataGrid)sender;
+
+            if (dataGrid.SelectedItem is not DataRowView Row_Selected)
+            {
+                return;
+            }
+            inpCountryId.Text = Row_Selected["country_Id"].ToString();
+            inpCountryCode.Text = Row_Selected["country_Code"].ToString();
+            inpCountryName.Text = Row_Selected["country_Name"].ToString();
+            inpCountryCurrencyId.Text = Row_Selected["country_Defaultcurrency_Id"].ToString();
+            cboxCountryCurrency.Text = Row_Selected["country_Defaultcurrency_Symbol"].ToString();
+        }
+        private void ToolbarButtonSave(object sender, RoutedEventArgs e)
+        {
+            if (inpCountryCode.Text != "")
+            {
+                Database dbCurrencyConnection = new()
+                {
+                    TableName = DatabaseCurrencyTable
+                };
+
+                dbCurrencyConnection.Connect();
+                dbCurrencyConnection.SqlCommandString = "currency_Id";
+                dbCurrencyConnection.SqlWhereString = "currency_Symbol = '" + cboxCountryCurrency.Text + "'";
+
+                long currencyId = dbCurrencyConnection.RetrieveSpecificIdFromMySqlData();
+                inpCountryCurrencyId.Text = currencyId.ToString();
+
+                Database dbConnection = new Database
+                {
+                    TableName = DatabaseTable
+                };
+
+                //Database dbConnection = new Database();
+                dbConnection.Connect();
+
+                dbConnection.SqlCommand = "UPDATE ";
+                dbConnection.SqlCommandString = " SET " +
+                    "country_Code = '" + inpCountryCode.Text + "', " +
+                    "country_Name = '" + inpCountryName.Text + "', " +
+                    "country_Defaultcurrency_Symbol = '" + cboxCountryCurrency.Text + "', " +
+                    "country_Defaultcurrency_Id = '" + currencyId + "' WHERE " +
+                    "country_Id = " + inpCountryId.Text + ";";
+
+                dbConnection.TableName = DatabaseTable;
+
+                _ = dbConnection.UpdateMySqlDataRecord();
+                DataTable dtCountryCodes = dbConnection.LoadMySqlData();
+
+                // Load the data from the database into the datagrid
+                CountryCode_DataGrid.DataContext = dtCountryCodes;
+
+                // Make sure the eddited row in the datagrid is selected
+                CountryCode_DataGrid.SelectedIndex = int.Parse(inpCountryId.Text) - 1;
+                CountryCode_DataGrid.Focus();
+            }
+        }
+
+        private void ToolbarButtonNew(object sender, RoutedEventArgs e)
+        {
+            Database dbConnection = new Database();
+            dbConnection.Connect();
+
+            dbConnection.SqlCommand = "INSERT INTO ";
+            dbConnection.SqlCommandString = "(country_Code,country_Name,country_Defaultcurrency_Id, country_Defaultcurrency_Symbol) VALUES('*','',0, '');";
+            dbConnection.TableName = DatabaseTable;
+            int ID = dbConnection.UpdateMySqlDataRecord();
+            inpCountryId.Text = ID.ToString();
+            DataTable dtCountryCodes = dbConnection.LoadMySqlData();
+
+            // Load the data from the database into the datagrid
+            CountryCode_DataGrid.DataContext = dtCountryCodes;
+            CountryCode_DataGrid.SelectedIndex = dtCountryCodes.Rows.Count - 1;
+            CountryCode_DataGrid.Focus();
+            inpCountryCode.Text = "";
+        }
+        private void ToolbarButtonDelete(object sender, RoutedEventArgs e)
+        {
+            Database dbConnection = new Database();
+            dbConnection.Connect();
+
+            // Get the selected row
+            int row = int.Parse(CountryCode_DataGrid.SelectedIndex.ToString());
+
+            int ID = int.Parse(inpCountryId.Text);
+            dbConnection.SqlCommand = "DELETE FROM ";
+            dbConnection.SqlCommandString = " WHERE country_Id = " + ID + ";";
+            dbConnection.TableName = DatabaseTable;
+            _ = dbConnection.UpdateMySqlDataRecord();
+            _ = dbConnection.LoadMySqlData();
+
+            DataTable dtCountryCodes = dbConnection.LoadMySqlData();
+
+            // Load the data from the database into the datagrid
+            CountryCode_DataGrid.DataContext = dtCountryCodes;
+
+            // Make sure the correct row in the datagrid is selected after deletion
+            
+            // if row is already ithe first row, it should be reselected
+            if(row == 0)
+            {
+                CountryCode_DataGrid.SelectedIndex = 0;
+            }
+            else
+            {
+                CountryCode_DataGrid.SelectedIndex = row - 1;
+            }
+            
+            CountryCode_DataGrid.Focus();
+        }
+
+        private void CommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+
         }
     }
 }
