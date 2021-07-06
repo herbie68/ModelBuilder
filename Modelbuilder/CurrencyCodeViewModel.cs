f4791b2e7010cbc8f92483d81fc752631f53cd04
herbie68 <herbert.nijkamp@gmail.com>
2021-07-06 22:27:17 +0200

    Created Currency page

---
+++ Modelbuilder/CurrencyCodeViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelbuilder
+{
+    internal class CurrencyCodeViewModel
+    {
+        public List<string> CurrencyCollection { get; set; }
+        private readonly string DatabaseCurrencyTable = "currency";
+        public CurrencyCodeViewModel()
+        {
+            CurrencyCollection = new List<string>{};
+
+            Database dbCurrencyConnection = new()
+            {
+                TableName = DatabaseCurrencyTable
+            };
+
+            //DataTable dataTable = new DataTable();
+            dbCurrencyConnection.SqlSelectionString = "currency_Symbol";
+            dbCurrencyConnection.SqlOrderByString = "currency_Id";
+            dbCurrencyConnection.TableName = DatabaseCurrencyTable;
+
+            DataTable dtCurrencySelection = dbCurrencyConnection.LoadSpecificMySqlData();
+
+            for (int i = 0; i < dtCurrencySelection.Rows.Count; i++)
+            {
+                CurrencyCollection.Add(dtCurrencySelection.Rows[i][0].ToString());
+            };
+        }
+    }
+}
--- Modelbuilder/metadataCurrency.xaml.cs
+++ Modelbuilder/metadataCurrency.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -21,14 +22,139 @@ namespace Modelbuilder
     /// </summary>
     public partial class metadataCurrency : Page
     {
+        private readonly string DatabaseTable = "currency";
         public metadataCurrency()
         {
             InitializeComponent();
+            DataContext = new CurrencyCodeViewModel();
+
+            Database dbConnection = new()
+            {
+                TableName = DatabaseTable
+            };
+
+            _ = new DataTable();
+
+            DataTable dtCountryCodes = dbConnection.LoadMySqlData();
+
+            // Load the data from the database into the datagrid
+            CurrencyCode_DataGrid.DataContext = dtCountryCodes;
+
+            // Make sure the first row in the datagrid is selected
+            CurrencyCode_DataGrid.SelectedIndex = 0;
+            CurrencyCode_DataGrid.Focus();
+
+        }
+        private void CurrencyCode_DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DataGrid dataGrid = (DataGrid)sender;
+
+            if (dataGrid.SelectedItem is not DataRowView Row_Selected)
+            {
+                return;
+            }
+            inpCurrencyId.Text = Row_Selected["currency_Id"].ToString();
+            inpCurrencyCode.Text = Row_Selected["currency_Code"].ToString();
+            inpCurrencyName.Text = Row_Selected["currency_Name"].ToString();
+            inpCurrencySymbol.Text = Row_Selected["currency_Symbol"].ToString();
+            inpCurrencyRate.Text = Row_Selected["currency_ConversionRate"].ToString();
         }
         private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
         {
             Regex regex = new Regex("[^0-9,]+");
             e.Handled = regex.IsMatch(e.Text);
         }
+        private void ToolbarButtonSave(object sender, RoutedEventArgs e)
+        {
+            if (inpCurrencyCode.Text != "")
+            {
+                Database dbConnection = new Database
+                {
+                    TableName = DatabaseTable
+                };
+
+                //Database dbConnection = new Database();
+                dbConnection.Connect();
+
+                dbConnection.SqlCommand = "UPDATE ";
+                dbConnection.SqlCommandString = " SET " +
+                    "currency_Code = '" + inpCurrencyCode.Text + "', " +
+                    "currency_Name = '" + inpCurrencyName.Text + "', " +
+                    "currency_Symbol = '" + inpCurrencySymbol.Text + "', " +
+                    "currency_Id = '" + inpCurrencyId + "' WHERE " +
+                    "currency_ConversionRate = " + inpCurrencyRate.Text + ";";
+
+                dbConnection.TableName = DatabaseTable;
+
+                _ = dbConnection.UpdateMySqlDataRecord();
+                DataTable dtCountryCodes = dbConnection.LoadMySqlData();
+
+                // Load the data from the database into the datagrid
+                CurrencyCode_DataGrid.DataContext = dtCountryCodes;
+
+                // Make sure the eddited row in the datagrid is selected
+                CurrencyCode_DataGrid.SelectedIndex = int.Parse(inpCurrencyId.Text) - 1;
+                CurrencyCode_DataGrid.Focus();
+            }
+        }
+
+        private void ToolbarButtonNew(object sender, RoutedEventArgs e)
+        {
+            Database dbConnection = new Database();
+            dbConnection.Connect();
+
+            dbConnection.SqlCommand = "INSERT INTO ";
+            dbConnection.SqlCommandString = "(currency_Code,currency_Name,currency_Id, currency_Symbol, currency_ConversionRate) VALUES('*','',0, '', 0);";
+            dbConnection.TableName = DatabaseTable;
+            int ID = dbConnection.UpdateMySqlDataRecord();
+            inpCurrencyId.Text = ID.ToString();
+            DataTable dtCurrencyCodes = dbConnection.LoadMySqlData();
+
+            // Load the data from the database into the datagrid
+            CurrencyCode_DataGrid.DataContext = dtCurrencyCodes;
+            CurrencyCode_DataGrid.SelectedIndex = dtCurrencyCodes.Rows.Count - 1;
+            CurrencyCode_DataGrid.Focus();
+            inpCurrencyCode.Text = "";
+        }
+        private void ToolbarButtonDelete(object sender, RoutedEventArgs e)
+        {
+            Database dbConnection = new Database();
+            dbConnection.Connect();
+
+            // Get the selected row
+            int row = int.Parse(CurrencyCode_DataGrid.SelectedIndex.ToString());
+
+            int ID = int.Parse(inpCurrencyId.Text);
+            dbConnection.SqlCommand = "DELETE FROM ";
+            dbConnection.SqlCommandString = " WHERE currency_Id = " + ID + ";";
+            dbConnection.TableName = DatabaseTable;
+            _ = dbConnection.UpdateMySqlDataRecord();
+            _ = dbConnection.LoadMySqlData();
+
+            DataTable dtCountryCodes = dbConnection.LoadMySqlData();
+
+            // Load the data from the database into the datagrid
+            CurrencyCode_DataGrid.DataContext = dtCountryCodes;
+
+            // Make sure the correct row in the datagrid is selected after deletion
+
+            // if row is already ithe first row, it should be reselected
+            if (row == 0)
+            {
+                CurrencyCode_DataGrid.SelectedIndex = 0;
+            }
+            else
+            {
+                CurrencyCode_DataGrid.SelectedIndex = row - 1;
+            }
+
+            CurrencyCode_DataGrid.Focus();
+        }
+
+        private void CommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+
+        }
+
     }
 }
