bffa5c6c7bf509523afbef937870589588b34ab3
Herbert Nijkamp <herbert.nijkamp@gmail.com>
2022-03-09 14:40:34 +0100

    Completed Import Time Entries

--- Modelbuilder/Views/ImportTimeEntry.xaml.cs
+++ Modelbuilder/Views/ImportTimeEntry.xaml.cs
@@ -14,6 +14,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Xml.Linq;
 
+using static Modelbuilder.HelperGeneral;
+
 namespace Modelbuilder;
 
 public partial class ImportTimeEntry : Page
@@ -65,7 +67,9 @@ public partial class ImportTimeEntry : Page
         dispTotalLinesCount.Text = lines.Count().ToString();
         var l = 0;
         var error=0;
-        var lineString = "";
+        var errorCount = 0;
+        var WorkTypeId = "";
+        var ProjectId = "";
 
         List<(int, int, string)> errorList = new(); // First element => Line number, Second element => Error code, Third element Line string
 
@@ -79,7 +83,7 @@ public partial class ImportTimeEntry : Page
             dispLineCount.Text = l++.ToString();
             string[] lineField = line.Split(";");
 
-            lineString = lineField[1].ToString () + "  " + lineField[4].ToString () + " : " + lineField[2].ToString().Substring(0,5) + "-" + lineField[3].ToString ().Substring ( 0, 5 ); 
+            var lineString = lineField[1].ToString () + "  " + lineField[4].ToString () + " : " + lineField[2].ToString().Substring(0,5) + "-" + lineField[3].ToString ().Substring ( 0, 5 ); 
 
             // Check if there is a record with the ProjectCode
             var projectCodeCheck = _helperGeneral.CheckForRecords ( HelperGeneral.DbProjectTable, new string[1, 3]
@@ -88,7 +92,7 @@ public partial class ImportTimeEntry : Page
             if(projectCodeCheck > 0)
             {
                 // Existing Projectcode, get the ProjectId
-                var ProjectId = _helperGeneral.GetValueFromTable ( HelperGeneral.DbProjectTable, new string[1, 3]
+                ProjectId = _helperGeneral.GetValueFromTable ( HelperGeneral.DbProjectTable, new string[1, 3]
                 {   { HelperGeneral.DbProjectTableFieldNameCode, HelperGeneral.DbProjectTableFieldTypeCode, lineField[0].ToString() } }, new string[1, 3]
                 {   { HelperGeneral.DbProjectTableFieldNameId, HelperGeneral.DbProjectTableFieldTypeId,"" } } );
 
@@ -97,7 +101,8 @@ public partial class ImportTimeEntry : Page
             else
             {
                 error = 1;
-                errorList.Add((l, 1, lineString));
+                errorCount++;
+                errorList.Add((l, 1, "Project: " + lineField[0].ToString() + ", " + lineString));
             }
 
             // Check if there is a record with the WorkTypeName
@@ -107,38 +112,32 @@ public partial class ImportTimeEntry : Page
             if (worktypeNameCheck > 0)
             {
                 // Excisting WorktypeName get WorktypeId
-                var WorkTypeId = _helperGeneral.GetValueFromTable ( HelperGeneral.DbWorktypeTable, new string[1, 3]
+                WorkTypeId = _helperGeneral.GetValueFromTable ( HelperGeneral.DbWorktypeTable, new string[1, 3]
                 {   {HelperGeneral.DbWorktypeTableFieldNameName, HelperGeneral.DbWorktypeTableFieldTypeName, lineField[4] } }, new string[1, 3]
                 {   { HelperGeneral.DbProjectTableFieldNameId, HelperGeneral.DbProjectTableFieldTypeId,"" } } );
             }
             else
             {
                 error = 1;
+                errorCount++;
                 errorList.Add ( (l, 2, lineString) );
             }
 
-            var StartTime = (int.Parse(line[2].ToString().Substring(0,2)) * 60) + int.Parse(line[2].ToString ().Substring ( 3, 2 ));
-            var EndTime = (int.Parse ( line[3].ToString ().Substring ( 0, 2 ) ) * 60) + int.Parse(line[3].ToString ().Substring ( 3, 2 ));
+            var StartTime = (int.Parse(lineField[2].ToString().Substring(0,2)) * 60) + int.Parse(lineField[2].ToString ().Substring ( 3, 2 ));
+            var EndTime = (int.Parse ( lineField[3].ToString ().Substring ( 0, 2 ) ) * 60) + int.Parse(lineField[3].ToString ().Substring ( 3, 2 ));
 
             // Check if Starttime s before Endtime
             if (StartTime >= EndTime)
             {
                 error = 1;
+                errorCount++;
                 errorList.Add ( (l, 3, lineString) );
             }
 
             if (error == 0)
             {
                 dispStatusLine.Text = "Toegevoegd: " + lineString;
-            }
-            else
-            {
-                dispStatusLine.Text = "Fout in regel " + l + " : " + lineString
-;            }
 
-            /*
-            if(error == 0)
-            {
                 _helperGeneral.InsertInTable(HelperGeneral.DbTimeTable, new string[5, 3]
                 {   { HelperGeneral.DbTimeTableFieldNameWorkDate, HelperGeneral.DbTimeTableFieldTypeWorkDate, lineField[1].ToString() },
                     { HelperGeneral.DbTimeTableFieldNameProjectId, HelperGeneral.DbTimeTableFieldTypeProjectId,ProjectId},
@@ -146,10 +145,39 @@ public partial class ImportTimeEntry : Page
                     { HelperGeneral.DbTimeTableFieldNameEndTime, HelperGeneral.DbTimeTableFieldTypeEndTime, lineField[3].ToString() },
                     { HelperGeneral.DbTimeTableFieldNameWorktypeId, HelperGeneral.DbTimeTableFieldTypeWorktypeId, WorkTypeId } });
             }
-            */
+            else
+            { dispStatusLine.Text = "Fout in regel " + l + " : " + lineString; }
 
             lineString = "";
             error = 0;
+        }
+        dispLineCount.Text = dispTotalLinesCount.Text;
+
+        dispStatusLine.Text = dispTotalLinesCount.Text + " regels verwerkt, " + errorCount.ToString() + " regels overgeslagen vanwege fouten.";
+        if(errorCount > 0)
+        {
+            var errorMessage = (int.Parse(dispTotalLinesCount.Text) - errorCount).ToString() + " regels correct ingelezen." + System.Environment.NewLine + errorCount.ToString() + " regels overgeslagen vanwege fouten." + System.Environment.NewLine + System.Environment.NewLine;
+
+            for (int i = 0; i < errorList.Count(); i++)
+            {
+                switch (errorList[i].Item2)
+                {
+                    case 1:
+                        errorMessage += "Foutieve project code";
+                        break;
+                    case 2:
+                        errorMessage += "Foutieve Werksoort naam";
+                        break;
+                    case 3:
+                        errorMessage += "Eindtijd eerder of gelijk dan de starttijd";
+                        break;
+                    default:
+                        errorMessage += "Onbekende fout";
+                        break;
+                }
+                errorMessage += " in regel " + errorList[i].Item1 + System.Environment.NewLine + "  >> " + errorList[i].Item3 + System.Environment.NewLine;
+            }
+            MessageBox.Show(errorMessage, "Fouten gevonden tijdens importeren", MessageBoxButton.OK, MessageBoxImage.Error);
         }
     }
 }
