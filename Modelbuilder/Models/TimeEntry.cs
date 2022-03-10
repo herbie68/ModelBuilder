7860521545133fc64ed30732f387a9a020dac609
Herbert Nijkamp <herbert.nijkamp@gmail.com>
2022-03-10 15:12:00 +0100

    Started workin on TimeView based on https://www.youtube.com/watch?v=S_0ZHvXCtYY&t=60s

---
+++ Modelbuilder/Models/TimeEntry.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelbuilder;
+
+public class TimeEntry
+{
+    public int ProjectId            { get; set; }
+    public string ProjectName       { get; set; }
+    public int WorktypeId           { get; set; }
+    public string WorktypeName      { get; set; }
+    public string EntryDate         { get; set; }
+    public string Year                 { get; set; }
+    public string YearMonth            { get; set; }
+    public string Month                { get; set; }
+    public int DayNo                { get; set; }
+    public string Day               { get; set; }
+    public string YearDay              { get; set; }
+    public string Worktime          { get; set; }
+    
+    public string NoGroup
+    {
+        get { return "Total"; }
+    }
+
+    public class TimeEntries : ObservableCollection<TimeEntry>
+    {
+        public TimeEntries()
+        {
+            this.Add(new TimeEntry
+            {
+                ProjectId = 1,
+                ProjectName = "SILHOUET",
+                WorktypeId = 1,
+                WorktypeName = "Afwerking",
+                EntryDate = "01-01-2021",
+                Year = "2021",
+                YearMonth = "202101",
+                Month = "01",
+                DayNo = 0,
+                Day = "Zondag",
+                YearDay = "20210",
+                Worktime = "04:30"
+            });
+            this.Add(new TimeEntry
+            {
+                ProjectId = 1,
+                ProjectName = "SILHOUET",
+                WorktypeId = 1,
+                WorktypeName = "Afwerking",
+                EntryDate = "02-01-2021",
+                Year = "2021",
+                YearMonth = "202101",
+                Month = "01",
+                DayNo = 1,
+                Day = "Maandag",
+                YearDay = "20211",
+                Worktime = "04:00"
+            });
+            this.Add(new TimeEntry
+            {
+                ProjectId = 1,
+                ProjectName = "SILHOUET",
+                WorktypeId = 2,
+                WorktypeName = "Dek",
+                EntryDate = "03-01-2021",
+                Year = "2021",
+                YearMonth = "202101",
+                Month = "01",
+                DayNo = 2,
+                Day = "Dinsdag",
+                YearDay = "20212",
+                Worktime = "03:00"
+            });
+            this.Add(new TimeEntry
+            {
+                ProjectId = 2,
+                ProjectName = "ZUIDERZEEBOTTER",
+                WorktypeId = 1,
+                WorktypeName = "Afwerking",
+                EntryDate = "01-01-2022",
+                Year = "2022",
+                YearMonth = "202201",
+                Month = "01",
+                DayNo = 1,
+                Day = "Maandag",
+                YearDay = "20221",
+                Worktime = "04:30"
+            });
+            this.Add(new TimeEntry
+            {
+                ProjectId = 1,
+                ProjectName = "ZUIDERZEEBOTTER",
+                WorktypeId = 1,
+                WorktypeName = "Afwerking",
+                EntryDate = "02-01-2022",
+                Year = "2022",
+                YearMonth = "202201",
+                Month = "01",
+                DayNo = 2,
+                Day = "Dinsdag",
+                YearDay = "20222",
+                Worktime = "04:00"
+            });
+            this.Add(new TimeEntry
+            {
+                ProjectId = 1,
+                ProjectName = "ZUIDERZEEBOTTER",
+                WorktypeId = 2,
+                WorktypeName = "Dek",
+                EntryDate = "03-01-2022",
+                Year = "2022",
+                YearMonth = "202201",
+                Month = "01",
+                DayNo = 3,
+                Day = "Woennsdag",
+                YearDay = "20223",
+                Worktime = "03:00"
+            });
+        }
+    }
+}
---
+++ Modelbuilder/ViewModels/TimeViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Input;
+
+namespace Modelbuilder;
+
+public class TimeViewModel
+{
+    public ICollectionView TimeView { get; set; }
+    public TimeViewModel()
+    {
+        //https://www.youtube.com/watch?v=S_0ZHvXCtYY&t=60s
+        IList<TimeEntry> entries = new TimeEntry();
+        TimeView = CollectionViewSource.GetDefaultView(entries);
+    }
+
+}
